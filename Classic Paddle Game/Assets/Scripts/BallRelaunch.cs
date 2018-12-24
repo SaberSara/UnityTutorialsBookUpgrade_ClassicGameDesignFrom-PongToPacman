2ea9710241529691af1885f8c7e960f394d02953
Sara Saber <1992kurai@gmail.com>
2018-12-24 16:02:55 +0100

    Add Classic Paddle Game Project
    Addig full Tutorial chapter
    Pages from 44-71

---
+++ Classic Paddle Game/Assets/Scripts/BallRelaunch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRelaunch : MonoBehaviour
+{
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.position.x > 0)
+            Scoring.scorep1++;
+        else
+            Scoring.scorep2++;
+        
+
+       //Added parts (Not workings good, uncomment! !
+        //Reseting the force and direction pushed into for the ball in each relaunch with a ranged randoom directions and a random added forcess for x and y ( like for the Ball script instantiation tn the beginnings of the game start mode)
+        //other.GetComponent<Rigidbody>().AddForce(0, 0, 0);
+        //other.GetComponent<Rigidbody>().AddForce(Random.Range(6, 8), Random.Range(-4, -3), 0);
+
+
+        other.transform.position = new Vector3(0, 0, 0);
+
+    }
+}
+
---
+++ Classic Paddle Game/Assets/Scripts/BallScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    //Attributs
+
+    public AudioClip BeepSound;
+
+
+    //Methodss
+    void Start()
+    {
+        GetComponent<Rigidbody>().freezeRotation = true;
+        //called the courotine to replace the old statement for better performancess>>
+        //>yield WaitForSeconds(3);
+        ///////
+        StartCoroutine(Delay());
+        GetComponent<Rigidbody>().AddForce(Random.Range(6, 8), Random.Range(-4, -3), 0);
+    }
+    void Update()
+    {
+        transform.position= transform.position+new Vector3(0,0,transform.position.z*0);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<AudioSource>().PlayOneShot(BeepSound);
+    }
+
+    //Added the courotine to replace the "wait for seconds in the book, for betters performancess
+    IEnumerator Delay()
+    {
+
+        yield return new WaitForSeconds(3);
+    }
+}
