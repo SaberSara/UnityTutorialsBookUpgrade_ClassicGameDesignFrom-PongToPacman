341483b37305c205ef8730e99f45e840fc506873
Sara Saber <1992kurai@gmail.com>
2018-12-27 17:46:55 +0100

    Revert "Adding Classical Brick Game Project"
    This reverts commit 1fe919b9d401515eeb0e8d23c2eee039a1822c01.

---
+++ Classic Paddle Game/Assets/Scripts/FinshingTheGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinshingTheGame : MonoBehaviour
+{
+    /// <AboutThisClass>
+    /// This is an added class to conclude the game on a score of 12 by either p1 or p2!
+    /// Check the Finished GAne GameObject in the hierarchy and the script attached to GameObject Score
+    /// </AboutThisClass>
+
+
+
+
+    //Attributs
+    public GameObject FGObject;
+    public GameObject BallObject;
+    public Text FGText;
+
+    //Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        FGObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Scoring.scorep1>=12) 
+        {
+            BallObject.SetActive(false);
+            FGObject.SetActive(true);
+            FGText.text = "Player 1 Won!";
+        }
+        else if(Scoring.scorep2 >= 12)
+        {
+            BallObject.SetActive(false);
+            FGObject.SetActive(true);
+            FGText.text = "Player 2 Won!";
+        }
+
+    }
+}
+
+//Thank you for reading
---
+++ Classic Paddle Game/Assets/Scripts/Scoring.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//Added is needed for the neww UI System (important ti to imports! before to use UI the new systems!!!!! !).
+
+public class Scoring : MonoBehaviour
+{
+    //Attributs
+    static public int scorep1;
+    static public int scorep2;
+    //the new UI Text to call
+    public Text P1Text;
+    public Text P2Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scorep1 = 0;
+        scorep2 = 0;
+        //Initialize the new UI objects Texts
+        P1Text.text = "Player 1:  " + scorep1;
+        P2Text.text = "Player 2: " + scorep2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        P1Text.text = "Player 1:  " + scorep1;
+        P2Text.text = "Player 2: " + scorep2;
+    }
+
+
+    //Gettuing replace for old GUI system by the new UI system from Unity 4.6
+    /// <note>
+    /// Here is the old code 
+    /// void OnGUI()
+    ///{
+    ///    GUI.Box(Rect(10, 10, 90, 30), "Player 1: " + scorep1);
+    ///    GUI.Box(Rect(Screen.width - 100, 10, 90, 30),
+    ///    "Player 2: " + scorep2);
+    ///}
+    /// 
+    /// </notey>
+
+}
